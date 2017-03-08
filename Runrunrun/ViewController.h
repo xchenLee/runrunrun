a5a7c7299eb265fd4856e0abe69cf7161de04251
xingchen.li <xingchen.li@renren-inc.com>
2017-03-08 14:23:10 +0800

    Initial Commit

---
+++ Runrunrun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runrunrun
+//
+//  Created by renren on 17/3/8.
+//  Copyright © 2017年 com.testss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Runrunrun/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Runrunrun
+//
+//  Created by renren on 17/3/8.
+//  Copyright © 2017年 com.testss. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
